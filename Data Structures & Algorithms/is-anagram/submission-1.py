345e6e3a9251cf3e620501dcff4e6cf095ae3447
shivangi29 <shivangigupta2908@gmail.com>
2026-06-05 15:50:25 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_count  = [0]*26
+        t_count  = [0]*26
+        
+        for i in range(len(s)):
+            s_count[ord(s[i]) - ord('a')] += 1
+            t_count[ord(t[i]) - ord('a')] += 1
+
+        return s_count == t_count
