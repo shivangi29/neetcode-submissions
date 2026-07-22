7f3e998b9de87a3145d2000222d7543885fa85fd
shivangi29 <shivangigupta2908@gmail.com>
2026-07-22 18:26:00 +0530

    Add: sort-an-array - submission-4

---
+++ Data Structures & Algorithms/sort-an-array/submission-4.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void mergeSort(int l, int r, vector<int>& nums) {
+        
+        if(l == r) return;
+        int m = (r+l)>>1;
+        mergeSort(l, m, nums);
+        mergeSort(m+1,r,nums);
+
+        vector<int> arr(r-l+1, 0);
+        int k = 0;
+        int i = l, j = m+1;
+        while(i<=m && j<=r) {
+            if(nums[i] > nums[j]) {
+                arr[k++] = nums[j++];
+            } else { 
+                arr[k++] = nums[i++];
+            }
+        }
+        while(i <= m) {
+            arr[k++] = nums[i++];
+        }
+        while(j <= r) {
+            arr[k++] = nums[j++];
+        }
+        k = 0;
+        for(i = l; i <= r; i++) {
+            nums[i] = arr[k++];
+        }
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(0,nums.size()-1,nums);
+        return nums;
+    }
+};
