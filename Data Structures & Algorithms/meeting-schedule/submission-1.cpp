9795d093114f2df20e374871be3636a29052f0c4
shivangi29 <shivangigupta2908@gmail.com>
2026-07-31 01:24:26 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    static bool compareIntervals(const Interval& a, const Interval& b) {
+        if(a.start == b.start)
+            return a.end<b.end;
+        return a.start<b.start;
+
+    }
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), compareIntervals);  
+        for(int i = 1; i < intervals.size(); i++){
+            if(intervals[i].start < intervals[i-1].end)
+                return false;
+        }
+        return true;
+    }
+};
