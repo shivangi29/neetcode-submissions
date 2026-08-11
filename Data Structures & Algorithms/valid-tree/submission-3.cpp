684b8f51f2f6c4eb362ac2c2afc1f6efc18fd2e1
shivangi29 <shivangigupta2908@gmail.com>
2026-08-11 18:14:48 +0530

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    //set<pair<int,int>> edge_vis;
+    vector<int> par, sz;
+    int count;
+    bool join(int i, int j) {
+        int pi = findPar(i);
+        int pj = findPar(j);
+        if(pi == pj) return false;
+        count--;
+        if(sz[pi] > sz[pj]) {
+            par[pj] = pi;
+            sz[pi] += sz[pj];
+        } else {
+            par[pi] = pj;
+            sz[pj] += sz[pi];
+        }
+        return true;
+    }
+    int findPar(int i) {
+        if(par[i] == i) return i;
+        return par[i] = findPar(par[i]);
+    }
+    bool validTree(int n, vector<vector<int>>& edges) {
+        par.resize(n);
+        sz.resize(n,1);
+        for(int i = 0 ; i < n; i++) {
+            par[i] = i;
+        }
+        count = n;
+        for(int i = 0 ; i < edges.size() ;i++) {
+            if(join(edges[i][0], edges[i][1]) == false)
+                return false; 
+        }
+        return count == 1;
+    }
+};
