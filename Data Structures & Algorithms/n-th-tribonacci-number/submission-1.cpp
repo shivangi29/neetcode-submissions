f845a7208473a6a1bedd2aff416535591a651d6f
shivangi29 <shivangigupta2908@gmail.com>
2026-08-15 13:16:30 +0530

    Add: n-th-tribonacci-number - submission-1

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n<=1) return n;
+        int a = 0, b = 1,c = 1;
+        for(int i = 3 ; i <=n ; i++){
+            int res = a+b+c;
+            a = b;
+            b = c;
+            c = res;   
+        }
+        return c;
+    }
+};
