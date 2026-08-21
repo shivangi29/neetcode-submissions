3a985537335bc9c2155a43d475dca6ea7f0db660
shivangi29 <shivangigupta2908@gmail.com>
2026-08-21 16:43:32 +0530

    Add: perfect-squares - submission-1

---
+++ Data Structures & Algorithms/perfect-squares/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int findNum(int sum, vector<int>& nums, vector<int>& dp) {
+        if(dp[sum] != -1) return dp[sum];
+     
+        int res = INT_MAX;
+        for(int num : nums) {
+            if(num > sum)
+                break;
+            if(num <= sum) {
+                res = min(res, 1 + findNum(sum-num, nums, dp));
+            }
+        }
+        return dp[sum] = res;
+    }
+    int numSquares(int n) {
+        vector<int> nums;
+        for(int i = 1;  i*i<=n ; i++) {
+            nums.push_back(i*i);
+        }
+        vector<int> dp(n+1, -1);
+        dp[0] = 0;
+        return findNum(n, nums, dp);
+    }
+};
