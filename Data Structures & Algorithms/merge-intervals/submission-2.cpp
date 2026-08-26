4fe7439ae3f0cf1af17524c5fb3c8cc2253d3803
shivangi29 <shivangigupta2908@gmail.com>
2026-08-26 20:11:53 +0530

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    static bool sortFunc(const vector<int>& a, const vector<int>& b) {
+        if(a[0] == b[0])
+            return a[1]<b[1];
+        return a[0]<b[0];
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        int i = 1;
+        int n = intervals.size();
+        int pos = 0;
+        while(i<n) {
+            int st = intervals[i][0];
+            int end = intervals[i][1];
+            if(st<=intervals[pos][1]) {
+                intervals[pos][0] = min(st,intervals[pos][0]),
+                intervals[pos][1] = max(end,intervals[pos][1]);
+            } else {
+                pos++;
+                intervals[pos] = intervals[i];
+            }
+            i++;
+        }
+        pos++;
+        while(pos<n) {
+            intervals.pop_back();
+            pos++;
+        }    
+        return intervals;
+    }
+};
